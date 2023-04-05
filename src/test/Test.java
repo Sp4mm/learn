0b9d033bad90e179b83a548f2fb48d17970baf11
Sp4mm <ezov.denis@ukr.net>
2023-04-05 13:28:16 +0300

    new tasks

--- src/test/Test.java
+++ src/test/Test.java
@@ -3,15 +3,70 @@ package test;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.*;
 
 
 public class Test {
     public static void main(String[] args) throws IOException {
 
-        int[] arr = new int[]{0, 1, 2, 3, 5, 6, 7};
+        arrayTask();
+    }
 
-        System.out.println(task2(arr));
+    public static void arrayTask() throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        List <Integer> list = new ArrayList<>();
+
+        String str = reader.readLine();
+
+        while (!str.equals("")){
+            list.add(Integer.parseInt(str));
+            str = reader.readLine();
+        }
+
+        for (int res : list) System.out.print(res + " ");
+    }
+
+    public static int task3(int[] array){
+
+        for (int i = 0; i < array.length - 1; i++) {
+
+
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] == array[j]){
+                    array[i] = -1;
+                    array[j] = -1;
+
+
+                }
+            }
+
+        }
+
+        for (int res : array) {
+            if (res != -1){
+                return res;
+            }
+        }
+        return -1;
+    }
+
+    public static int findUnique(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : arr) {
+            if (map.containsKey(i)) {
+                map.put(i, map.get(i) + 1);
+            } else {
+                map.put(i, 1);
+            }
+        }
+        for (int i : arr) {
+            if (map.get(i) == 1) {
+                return i;
+            }
+        }
+        return -1; // если уникальный элемент не найден
     }
 
     public static int task2(int[] array){
