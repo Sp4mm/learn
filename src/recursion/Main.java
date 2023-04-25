b192592140df0a47e679253a6ca2b113fe705a79
Sp4mm <ezov.denis@ukr.net>
2023-04-26 01:11:23 +0300

    "recursion" changes

--- src/recursion/Main.java
+++ src/recursion/Main.java
@@ -7,8 +7,42 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(fibonacci(10));
+        int[] arr = new int[10];
+        for (int i = 0; i < 10; i++) {
+            arr[i] = i * 10;
+        }
+        System.out.println(binarySearch(arr, 60));
     }
+
+    public static int binarySearch(int[] array, int n, int first, int last){
+        int mid = (first + last) / 2;
+        if (array[mid] == n) return mid;
+        if (array[mid] < n) return binarySearch(array, n, mid + 1, last);
+        return binarySearch(array, n, first, last - 1);
+    }
+
+    public static int binarySearch(int[] array, int n){
+        int first = 0;
+        int last = array.length - 1;
+        return binarySearch(array, n, first, last);
+    }
+
+    /*public static int binarySearch(int[] array, int n){
+        int first = 0;
+        int last = array.length - 1;
+        int mid = first + last / 2;
+
+        while (first < last){
+            if (array[mid] == n) {
+                return mid;
+            }else if (array[mid] < n) {
+                last = mid - 1;
+            }else {
+                first = mid + 1;
+            }
+        }
+        return mid;
+    }*/
 
     /*public static BigInteger fibonacci(int n){
         BigInteger res = BigInteger.ZERO;
